80ef8a8d54761d0cffe26e4c49818aa0752a2841
S David Kong <SDAVIDKONG@gmail.com>
2022-04-29 11:26:15 +0300

    adding react logo

--- src/index.js
+++ src/index.js
@@ -1,7 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+const frontpage = (
+    <div>
+        <img src=".\react-logo.png" width="40px"/>
+        <h1>Fun facts about React</h1>
+        <ul>
+            <li>Was first released in 2013</li>
+            <li>Was originally created by Jordan Walke</li>
+            <li>Has Well over 100k stars on Github</li>
+            <li>Is maintained by Facebook</li>
+            <li>Powers thousands of enterprise apps, including mobile apps</li>
+        </ul>
 
-ReactDOM.render(<h1>gm world</h1>, document.getElementById("root"));
+    </div>
+)
+
+
+ReactDOM.render(frontpage, document.getElementById("root"));
 
 
