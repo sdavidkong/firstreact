c08a4968c32f7cc57f729f4df90e33a7f3f35610
S David Kong <SDAVIDKONG@gmail.com>
2022-05-02 14:25:55 +0300

    separation of components

---
+++ src/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Footer() {
+    return (
+        <footer>
+            <small>© 2021 Kong development. All rights reserved.</small>
+        </footer>
+    )
+}
+
+
+
+export default function Footer();
--- src/index.js
+++ src/index.js
@@ -1,22 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
-const frontpage = (
-    <div>
-        <img src="./react-logo.png" width="40px"/>
-        <h1>Fun facts about React</h1>
-        <ul>
-            <li>Was first released in 2013</li>
-            <li>Was originally created by Jordan Walke</li>
-            <li>Has Well over 100k stars on Github</li>
-            <li>Is maintained by Facebook</li>
-            <li>Powers thousands of enterprise apps, including mobile apps</li>
-        </ul>
-
-    </div>
-)
+import Header from "./Header";
 
 
-ReactDOM.render(frontpage, document.getElementById("root"));
+function Page() {
+    return (
+        <div>
+            <Header />
+            <MainContent />
+            <Footer />
+        </div>
+    )
+}
 
+ReactDOM.render(<Page />, document.getElementById("root"))
 
